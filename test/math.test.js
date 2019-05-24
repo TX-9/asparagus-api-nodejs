cf4dcea879b6b61de3241334ecf2e75cad72624d
TX-9 <jacobleecs@gmail.com>
2019-05-24 15:06:53 -0400

    add Testing Framework Jest

---
+++ test/math.test.js
@@ -0,0 +1,44 @@
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, add } = require('../src/math');
+
+test('Should calcuate total with tip', () => {
+    const total = calculateTip(10, .3);
+    expect(total).toBe(13);
+    // if (total !== 13) {
+    //     throw new Error('Total should be 13. Got ' + total);
+    // }
+});
+
+test('Should calcuate total with default tip', () => {
+    const total = calculateTip(10);
+    expect(total).toBe(12.5);  
+});
+
+test('Should convert 32 F to 0 C', () => {
+    const temp = fahrenheitToCelsius(32);
+    expect(temp).toBe(0);  
+});
+
+test('Should convert 0 C to 32 F', () => {
+    const temp = celsiusToFahrenheit(0);
+    expect(temp).toBe(32);  
+});
+
+// test('Async test demo', (done) => {
+//     setTimeout(() => {
+//         expect(1).toBe(2);
+//         done();
+//     }, 2000)
+// });
+
+test('Should add two number', (done) => {
+    add(2,3).then((sum) => {
+        expect(sum).toBe(5);
+        done();
+    })
+});
+
+test('Should add two number async/await', async () => {
+    const sum = await add(2,3);
+    expect(sum).toBe(5);
+        
+});
