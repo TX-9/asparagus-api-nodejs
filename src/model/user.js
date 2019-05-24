f409e05af873ec4e9437349d2df17f1fa54e6b95
TX-9 <jacobleecs@gmail.com>
2019-05-24 12:00:38 -0400

    add file upload feature

--- src/model/user.js
+++ src/model/user.js
@@ -47,7 +47,12 @@ const userSchema = new mongoose.Schema({
             type: String,
             required: true
         }
-    }]
+    }],
+    picture: {
+        type: Buffer
+    }
+}, {
+    timestamps: true
 });
 
 // moongoose's virtual property: relationship between two entity
--- src/router/user.js
+++ src/router/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('../model/user');
 const auth = require('../middleware/auth');
 const { sendWelcomeEmail, sendCancelationEmail } = require('../email/account');
@@ -130,6 +131,36 @@ const upload = multer({
 
         callback(undefined, true);
     }
+})
+
+router.post('/users/me/picture', auth, upload.single('picture'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer();
+    req.user.picture = buffer;
+    await req.user.save();
+    res.send();
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.mesaage });
+});
+
+router.get('/users/:id/picture', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (!user || !user.picture) {
+            throw new Error();
+        }
+
+        res.set('Content-Type', 'image/png');
+        res.send(user.picture);
+    } catch (e) {
+        res.status(404).send();
+    }
+});
+
+router.delete('/users/me/picture', auth, async (req, res) => {
+    req.user.picture = undefined;
+    await req.user.save();
+    res.send();
 })
 
 module.exports = router;
