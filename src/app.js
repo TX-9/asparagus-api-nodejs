5b34348cf8f47bb9bf29535e29251e55ddfea134
TX-9 <jacobleecs@gmail.com>
2019-05-29 14:07:04 -0400

    add home router to serve index.html

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 require('./db/mongoose');
+const homeRouter = require('./router/home');
 const userRouter = require('./router/user');
 const planRouter = require('./router/mealPlan');
 
 const app = express();
 app.use(express.json());
+app.use(homeRouter);
 app.use(userRouter);
 app.use(planRouter);
 
