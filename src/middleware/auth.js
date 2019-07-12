b1ce4d6678247e5f1e733cd76a1274dcc68e1099
TX-9 <jacobleecs@gmail.com>
2019-07-11 22:00:00 -0400

    bypass auth and set mock User

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,18 +4,26 @@ const User = require('../model/user');
 
 const auth = async (req, res, next) => {
     try {
-        const token = req.header('Authorization').replace('Bearer','').trim();
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
-
+        // bypass auth for testing  
+        const user = await User.findOne({ _id: "5d27df28602f3b3d609af987"});
         if(!user) {
             throw new Error();
         }
-
-        req.token = token;
         req.user = user;
         next();
-        console.log('user:' + user);
+
+        // const token = req.header('Authorization').replace('Bearer','').trim();
+        // const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        // const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+
+        // if(!user) {
+        //     throw new Error();
+        // }
+
+        // req.token = token;
+        // req.user = user;
+        // next();
+        //console.log('user:' + user);
     } catch (e) {
         res.status(401).send({error: 'Please authenticate'})
     }
