26a9e6eb420d1713ff0751250f2717ec77fa8b1c
TX-9 <jacobleecs@gmail.com>
2019-07-21 17:07:53 -0400

    add CORS

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 require('./db/mongoose');
 const homeRouter = require('./router/home');
 const userRouter = require('./router/user');
 const planRouter = require('./router/schedule');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(homeRouter);
 app.use(userRouter);
